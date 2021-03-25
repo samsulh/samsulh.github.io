6507145007b5d0c6883a3726f54cbf54e8ed9493
Samsul Hoque <samsul.h3.14@gmail.com>
2021-03-25 12:23:35 -0400

    completed fan-art page

--- script.js
+++ script.js
@@ -33,6 +33,49 @@ function removeActiveClasses() {
   });
 }
 
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active2");
+  });
+
+  slides[activeSlide].classList.add("active2");
+}
+
 let d = new Date();
 let y = d.getFullYear();
 document.getElementById("yearID").innerHTML = y;
